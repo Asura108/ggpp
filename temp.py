5a3035d2ac8fa16761bf2f79409b586cac1ad580
Deepanshu Molasi <85053641+Asura108@users.noreply.github.com>
2022-06-09 10:39:50 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pickle 
+
+loaded_model = pickle.load(open('F:/PythonProjectDeploying', 'rb'))
+
+new_list = [60,34,60000,234,1]
+
